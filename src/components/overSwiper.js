ec1e4a107c7f01c397530f49228ea9e8fc096e55
RubinVelcani <rubin.velcani@gmail.com>
2021-05-19 16:58:17 +0200

    'fixed link to contact from swiper'

--- src/components/overSwiper.js
+++ src/components/overSwiper.js
@@ -6,7 +6,7 @@ const OverSwiper = () => {
         <div className='flex flex-col items-center bg-overSwiper text-center py-10 px-20'>
             <h2 className='text-4xl font-semibold mb-4'>Mata Embroidery</h2>
             <h4 className='text-2xl mb-8'>Professional embroidery based in Tirana, Albania with over 40 years of experience</h4>
-            <Link to='/contactUs'>
+            <Link to='/contact'>
             <h4 className='border-gray-800 border py-4 px-12 mx-auto'>Get Started!</h4>
             </Link>
         </div>
